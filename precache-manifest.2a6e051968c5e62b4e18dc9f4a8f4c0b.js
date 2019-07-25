1ee46151d340fe8586e792b842f6918d3656e18c
Isaac Heist <isaacheist@gmail.com>
2019-07-24 17:13:32 -0700

    Updates

---
+++ precache-manifest.2a6e051968c5e62b4e18dc9f4a8f4c0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761ef812945c9ffd8bac555d7acbccb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a47777a2097954b5ffa",
+    "url": "/static/js/2.96c3d440.chunk.js"
+  },
+  {
+    "revision": "f9e279f8bf8be8208a82",
+    "url": "/static/js/main.5d7bb69c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
